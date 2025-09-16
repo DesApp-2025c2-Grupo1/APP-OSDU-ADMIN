20346a944fde27ff01552c9921dcd26c827d50f5
Matias <mendoza.e.matias@gmail.com>
2025-09-16 19:33:46 -0300

    feat: Agrego primeros componentes base

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <>
+            <div className='border-b-1 p-2'>
+                <h1 className='text-[#5FA92C] text-2xl font-semibold'>MEDIUNAHUR</h1>
+            </div>
+        </>
+    )
+}
