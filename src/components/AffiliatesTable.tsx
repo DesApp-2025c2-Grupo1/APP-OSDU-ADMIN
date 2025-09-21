d860dedd4265b9bb4308ae12084724ee4763c97f
Jess Tejero <jessy.tejero@hotmail.com>
2025-09-21 20:05:36 -0300

    se realizo el Popup de editar afiliado

--- src/components/AffiliatesTable.tsx
+++ src/components/AffiliatesTable.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { OptionsMenu } from "./OptionsMenu"; 
+import React, { useState } from "react";
+import { OptionsMenu } from "./OptionsMenu";
+import { EditAffiliatePopup } from "./EditAffiliatePopup";
 
 export type Affiliate = {
   credencial: string;
@@ -9,6 +10,15 @@ export type Affiliate = {
   fechaNacimiento: string;
   plan: string;
   direccion: string;
+  tipoDocumento?: string;
+  nroDocumento?: string;
+  planMedico?: string;
+  telefono?: string;
+  telefono2?: string;
+  email?: string;
+  email2?: string;
+  direccion2?: string;
+  situaciones?: Array<{situacion: string, fechaFinalizacion: string}>;
 };
 
 interface AffiliatesTableProps {
@@ -16,41 +26,62 @@ interface AffiliatesTableProps {
 }
 
 export function AffiliatesTable({ affiliates }: AffiliatesTableProps) {
-  
+  const [selectedAffiliate, setSelectedAffiliate] = useState<Affiliate | null>(null);
+  const [showEditPopup, setShowEditPopup] = useState(false);
+
   const handleOptionClick = (option: string, affiliate: Affiliate) => {
     console.log(`Opción seleccionada: ${option} para ${affiliate.nombre} ${affiliate.apellido}`);
+    
+    if (option === "Editar") {
+      setSelectedAffiliate(affiliate);
+      setShowEditPopup(true);
+    }
+  };
+
+  const handleSaveAffiliate = (data: any) => {
+    console.log("Datos guardados:", data);
   };
 
   return (
-    <table style={{ width: "100%", borderCollapse: "collapse" }}>
-      <thead>
-        <tr style={{ background: "#5FA92C", color: "white" }}>
-          <th>Credencial</th>
-          <th>DNI</th>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Fecha De Nac.</th>
-          <th>Plan</th>
-          <th>Dirección</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {affiliates.map((a) => (
-          <tr key={a.credencial}>
-            <td>{a.credencial}</td>
-            <td>{a.dni}</td>
-            <td>{a.nombre}</td>
-            <td>{a.apellido}</td>
-            <td>{a.fechaNacimiento}</td>
-            <td>{a.plan}</td>
-            <td>{a.direccion}</td>
-            <td>
-              <OptionsMenu affiliate={a} onOptionClick={handleOptionClick} />
-            </td>
+    <>
+      <table style={{ width: "100%", borderCollapse: "collapse" }}>
+        <thead>
+          <tr style={{ background: "#5FA92C", color: "white" }}>
+            <th>Credencial</th>
+            <th>DNI</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Fecha De Nac.</th>
+            <th>Plan</th>
+            <th>Dirección</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {affiliates.map((a) => (
+            <tr key={a.credencial}>
+              <td>{a.credencial}</td>
+              <td>{a.dni}</td>
+              <td>{a.nombre}</td>
+              <td>{a.apellido}</td>
+              <td>{a.fechaNacimiento}</td>
+              <td>{a.plan}</td>
+              <td>{a.direccion}</td>
+              <td>
+                <OptionsMenu affiliate={a} onOptionClick={handleOptionClick} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {showEditPopup && selectedAffiliate && (
+        <EditAffiliatePopup
+          affiliate={selectedAffiliate}
+          onClose={() => setShowEditPopup(false)}
+          onSave={handleSaveAffiliate}
+        />
+      )}
+    </>
   );
 }
